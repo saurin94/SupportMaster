792f6bfc7689e2a26b4aa86b1efc792190c6a1cc
Saurin Shah <shah.saurin@husky.neu.edu>
2018-09-20 16:47:17 -0400

    Added project strucuture.

---
+++ src/main/java/com/crud/app/reactjsspring/ReactjsSpringApplication.java
@@ -0,0 +1,12 @@
+package com.crud.app.reactjsspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReactjsSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReactjsSpringApplication.class, args);
+	}
+}
---
+++ src/test/java/com/crud/app/reactjsspring/ReactjsSpringApplicationTests.java
@@ -0,0 +1,16 @@
+package com.crud.app.reactjsspring;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ReactjsSpringApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
